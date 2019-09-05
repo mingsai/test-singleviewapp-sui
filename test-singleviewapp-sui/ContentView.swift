00fca6e0ffdd825a0f5ccda170ea72c1aea48db4
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-09-05 00:24:50 -0400

    Initial Commit

---
+++ test-singleviewapp-sui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test-singleviewapp-sui
+//
+//  Created by Tommie Carter on 9/5/19.
+//  Copyright © 2019 MING Technology LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test-singleviewapp-suiTests/test_singleviewapp_suiTests.swift
@@ -0,0 +1,34 @@
+//
+//  test_singleviewapp_suiTests.swift
+//  test-singleviewapp-suiTests
+//
+//  Created by Tommie Carter on 9/5/19.
+//  Copyright © 2019 MING Technology LLC. All rights reserved.
+//
+
+import XCTest
+@testable import test_singleviewapp_sui
+
+class test_singleviewapp_suiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
